b15a57833902db339d781d1bdf01a7585e4f9676
Jaden <m0de@null.net>
2015-11-22 23:36:39 -0500

    log4j
    Another log4j related commit.

---
+++ src/main/java/com/azureweb/azure/Azure.java
@@ -0,0 +1,12 @@
+package com.azureweb.azure;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Azure {
+    private static final Logger logger = LogManager.getLogger(Azure.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
